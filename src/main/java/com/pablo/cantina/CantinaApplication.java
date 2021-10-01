012f402c9e83fc67fc0672064bb3273ad06d32c1
pablowinck <pablowinck123@gmail.com>
2021-09-30 21:46:12 -0300

    init

---
+++ src/main/java/com/pablo/cantina/CantinaApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.cantina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/cantina/CantinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.cantina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
